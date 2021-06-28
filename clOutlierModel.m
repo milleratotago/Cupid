e6d8f571aa507d2544ad32d5f9cf9eb2c17aab56
milleratotago <miller@psy.otago.ac.nz>
2021-06-29 11:26:34 +1200

    added clOutlierModel

---
+++ clOutlierModel.m
@@ -0,0 +1,193 @@
+classdef clOutlierModel < handle
+    % Model scores in N separate conditions "contaminated" by a common outlier process
+    % that "contaminates" scores in one of NTypes possible ways (see Enum below).
+    % In all cases, the parameters of an object of this class are
+    % stored in the order: TrueDists{i} parms, prOutlier, Contam parms.
+    
+    properties(Constant)
+        NTypes  = 3;  % N of types enumerated here.
+        % Enum of different types of contamination:
+        Shift   = 1;  % Contamination RV adds to true score
+        Stretch = 2;  % Contamination RV multiplies times true score
+        Replace = 3;  % Contamination RV replaces true score
+    end
+    
+    properties
+        
+        % "Input" properties defining the model:
+        TrueDists  % Cell array of Cupid distributions of true ("uncontaminated") scores in the N conditions.
+        prOutlier  % Outlier probability, constant across conditions.
+        Contam     % Cupid distribution representing trial-to-trial variation in the extent of contamination.
+        ConType    % Type of contamination: Shift, Stretch, or Replace
+        
+        % Optional input parameters:
+
+        % Properties derived from the input properties:
+        NConds     % N of conditions described by the model
+        ObsDists  % Cell array of Cupid distributions of observed scores in the N conditions, including outliers.
+        
+        DefaultParmCodes
+        SearchOptions
+        SplinePDFs
+        
+        % Indices of parameters within the overall parameter list:
+        TrueParmIndices   % Cell array 1:NConds with indices of each distribution's parameters
+        prOutlierIndex    % Index of the prOutlier parameter
+        ContamParmIndices % Vector of indices of the parameters for the Contam distribution
+        TotalNParms
+        
+    end
+
+    methods
+        
+        function obj = clOutlierModel(TrueDists,prOutlier,Contam,ConType,varargin)
+            % Create object with specified values of input parameters.
+            assert(ConType >= 1 && ConType <= clOutlierModel.NTypes && ConType == floor(ConType),'Illegal ConType');
+            assert(prOutlier > 0 && prOutlier < 1,'prOutlier must be between 0 & 1');
+            [obj.SearchOptions, varargin] = ExtractNameVali('SearchOptions',optimset,varargin);
+            [obj.SplinePDFs, varargin] = ExtractNameVali({'SplinePDF','SplinePDFs'},0,varargin);
+            assert(numel(varargin)==0,'Unrecognized argument');
+            if iscell(TrueDists)
+                obj.TrueDists = TrueDists;
+            else
+                % Allow user to specify a single true distribution simply as a Cupid
+                % distribution rather than as a 1-position cell array.
+                obj.TrueDists = {TrueDists};
+            end
+            obj.prOutlier = prOutlier;
+            obj.Contam = Contam;
+            obj.ConType = ConType;
+            obj.NConds = numel(TrueDists);
+            obj.MakeObsDists;
+            
+            % Construct default parmcodes
+            sParmCodes = '';
+            for iCond = 1:obj.NConds
+                sParmCodes = [sParmCodes obj.TrueDists{iCond}.DefaultParmCodes]; %#ok<AGROW>
+            end
+            sParmCodes = [sParmCodes 'r' obj.Contam.DefaultParmCodes];
+            obj.DefaultParmCodes = sParmCodes';  % ' to match with column vector whose size is checked in fminsearcharb
+            
+            % Compute the indices of all parameters within the overall parameter lists
+            UsedParms = 0;
+            obj.TrueParmIndices = cell(obj.NConds,1);
+            for iCond=1:obj.NConds
+                obj.TrueParmIndices{iCond} = (UsedParms+1):(UsedParms+obj.TrueDists{iCond}.NDistParms);
+                UsedParms = UsedParms + obj.TrueDists{iCond}.NDistParms;
+            end
+            obj.prOutlierIndex  = UsedParms + 1;
+            obj.ContamParmIndices = (UsedParms+2):UsedParms+1+obj.Contam.NDistParms;
+            obj.TotalNParms = obj.ContamParmIndices(end);
+            
+        end
+        
+        function [] = MakeObsDists(obj)
+            % Initialize the observed distributions with the current parameter values.
+            obj.ObsDists = cell(obj.NConds,1);
+            for iCond=1:obj.NConds
+                switch obj.ConType
+                    case obj.Shift
+                        obj.ObsDists{iCond} = ContamShift(obj.TrueDists{iCond},obj.prOutlier,obj.Contam);
+                    case obj.Stretch
+                        obj.ObsDists{iCond} = ContamStretch(obj.TrueDists{iCond},obj.prOutlier,obj.Contam);
+                    case obj.Replace
+                        obj.ObsDists{iCond} = Mixture(1-obj.prOutlier,obj.TrueDists{iCond},obj.Contam);
+                end
+                if obj.SplinePDFs > 0
+                    obj.ObsDists{iCond}.UseSplinePDFOn(obj.SplinePDFs);
+                end
+            end
+        end
+        
+        function Observed = Random(obj,varargin)
+            % Generate random observed scores with current model parameters.
+            % Observed is a cell array, 1:NConds, of scores in the different conditions.
+            Observed = cell(obj.NConds,1);
+            for iCond = 1:obj.NConds
+                Observed{iCond} = obj.ObsDists{iCond}.Random(varargin{:});
+            end
+        end
+
+        function ll = LnLikelihood(obj,X)
+            % Compute likelihood of the data in X with current model parameters.
+            % X is a cell array, 1:NConds, of scores in the different conditions.
+            perCondll = zeros(obj.NConds,1);
+            for iCond = 1:obj.NConds
+                perCondll(iCond) = obj.ObsDists{iCond}.LnLikelihood(X{iCond});
+            end
+            ll = sum(perCondll);
+        end
+        
+        function [EndingVals,fval,exitflag,output]=EstML(obj,X,varargin)
+            % Estimate distribution parameters by maximum likelihood [i.e., minimize -log(likelihood)]
+            % X is a cell array, 1:NConds, of observed scores in the different conditions.
+            % exitflags: 1 converged, 0 N of iterations exceeded, -1 terminated by output function.
+             if numel(varargin)<1
+                ParmCodes = obj.DefaultParmCodes;
+            else
+                ParmCodes = varargin{1};
+            end
+            Real2ParmFn = @obj.RealsToParms;
+            Parm2ReamFn = @obj.ParmsToReals;
+            ErrFn = @MyErrFunc;
+            StartingVals = ParmValues(obj);
+            [EndingVals,fval,exitflag,output] = fminsearcharb(ErrFn,StartingVals,Real2ParmFn,Parm2ReamFn,ParmCodes,obj.SearchOptions);
+            obj.ResetParms(EndingVals);
+           
+             function thiserrval=MyErrFunc(realParms)
+                ResetParms(obj,realParms)
+                thiserrval = -LnLikelihood(obj,X);
+            end
+            
+        end
+        
+        function ResetParms(obj,newparmvalues)
+            for iCond=1:obj.NConds
+                theseIndices = obj.TrueParmIndices{iCond};
+               obj.TrueDists{iCond}.ResetParms(newparmvalues(theseIndices));
+            end
+            obj.prOutlier = newparmvalues(obj.prOutlierIndex);
+            obj.Contam.ResetParms(newparmvalues(obj.ContamParmIndices));
+            obj.MakeObsDists;
+        end
+        
+        function parms = ParmValues(obj)
+            % Retrieve the current parameter values
+            parms = zeros(obj.TotalNParms,1);
+            for iCond=1:obj.NConds
+                theseIndices = obj.TrueParmIndices{iCond};
+                parms(theseIndices) = obj.TrueDists{iCond}.ParmValues;
+            end
+            parms(obj.prOutlierIndex) = obj.prOutlier;
+            parms(obj.ContamParmIndices) = obj.Contam.ParmValues;
+        
+        end
+        
+        function realList = ParmsToReals(obj,Parms,~)
+            % Return a list of the model parameter values transformed into reals as appropriate for fminsearch.
+            % These come out in the order: TrueDists parms, prOutlier, Contam parms.
+            realList = zeros(obj.TotalNParms,1);
+            for iCond = 1:obj.NConds
+                theseIndices = obj.TrueParmIndices{iCond};
+                realList(theseIndices) = obj.TrueDists{iCond}.ParmsToReals(Parms(theseIndices));
+            end
+            realList(obj.prOutlierIndex) = NumTrans.Bounded2Real(0,1,Parms(obj.prOutlierIndex));
+            realList(obj.ContamParmIndices) = obj.Contam.ParmsToReals(Parms(obj.ContamParmIndices));
+        end
+      
+        function parmList = RealsToParms(obj,Reals,~)
+            % Return a list of the model parameter values retrieved from the reals used by fminsearch.
+            % These come out in the order: TrueDists parms, prOutlier, Contam parms.
+            parmList = zeros(obj.TotalNParms,1);
+            for iCond = 1:obj.NConds
+                theseIndices = obj.TrueParmIndices{iCond};
+                parmList(theseIndices) = obj.TrueDists{iCond}.RealsToParms(Reals(theseIndices));
+            end
+            parmList(obj.prOutlierIndex) = NumTrans.Real2Bounded(0,1,Reals(obj.prOutlierIndex));
+            parmList(obj.ContamParmIndices) = obj.Contam.RealsToParms(Reals(obj.ContamParmIndices));
+        end
+
+    end % methods
+    
+end % clOutlierModel
+
---
+++ clOutlierModelDemo.m
@@ -0,0 +1,86 @@
+% Demo of clOutlierModel
+
+prOutlier = 0.1;   % Probability that a true score is affected by the outlier process.
+
+%% Example 1:  A single true distribution, sometimes shifted by an outlier process.
+
+TrueDist = Normal(400,40);       % The distribution of true scores
+ConType = clOutlierModel.Shift;  % In this model, contamination adds a random value to the true score
+Contam = Normal(400,10);         % This is the distribution of the amount added
+
+% Instantiate the model for the possibly-contaminated scores:
+ModelWithContam = clOutlierModel(TrueDist,prOutlier,Contam,ConType);
+
+% View the PDFs of the true scores and the observed (including contamination) scores:
+figure; plotDists({ ModelWithContam.TrueDists{1}, ModelWithContam.ObsDists{1} },1);
+
+% Generate some random observed scores.
+% These are a mixture of True and Contaminated (shifted true) scores
+X = ModelWithContam.Random(200,1);
+
+% Example of parameter search (this takes a minute or two):
+[EndingVals,fval,exitflag,output] = ModelWithContam.EstML(X)
+
+%% Example 2:  Two different true distributions, each sometimes shifted by an outlier process.
+
+TrueDists = {Normal(400,40), Normal(450,45)};       % CELL ARRAY with he distributions of true scores in two conditions
+ConType = clOutlierModel.Shift;  % In this model, contamination adds a random value to the true score
+Contam = Normal(400,35);         % This is the distribution of the amount added
+
+% Instantiate the model for the possibly-contaminated scores:
+ModelWithContam = clOutlierModel(TrueDists,prOutlier,Contam,ConType);
+
+% View the PDFs of the true scores and the observed (including contamination) scores:
+figure; plotDists({ ModelWithContam.TrueDists{1}, ModelWithContam.TrueDists{2}, ...
+    ModelWithContam.ObsDists{1}, ModelWithContam.ObsDists{2} },1);
+
+% Generate some random observed scores.
+% These are a mixture of True and Contaminated (shifted true) scores
+X = ModelWithContam.Random(500,1);
+
+% Example of parameter search (this takes a minute or two):
+[EndingVals,fval,exitflag,output] = ModelWithContam.EstML(X)
+
+
+%% Example 3: 2 conditions, contamination by stretching.
+% (all RVs must be strictly positive because CUPID's Product RV does not all 0 or negative values):
+
+prOutlier = 0.2;   % Probability that a true score is affected by the outlier process.
+
+TrueDists = {Beta(10,20), Beta(12,22)};
+Contam = Triangular(1.3,1.5);
+ConType = clOutlierModel.Stretch;
+ModelWithContam = clOutlierModel(TrueDists,prOutlier,Contam,ConType);
+
+% View the PDFs of the true scores and the observed (including contamination) scores:
+figure; plotDists({ ModelWithContam.TrueDists{1}, ModelWithContam.TrueDists{2}, ...
+    ModelWithContam.ObsDists{1}, ModelWithContam.ObsDists{2} },1);
+
+% Generate some random observed scores.
+% These are a mixture of True and Contaminated (shifted true) scores
+X = ModelWithContam.Random(500,1);
+
+% Example of parameter search (this takes 5-10 minutes, because Product is slow):
+[EndingVals,fval,exitflag,output] = ModelWithContam.EstML(X)
+
+
+
+%% Example 4: 2 conditions, contamination by replacement:
+
+prOutlier = 0.15;   % Probability that a true score is affected by the outlier process.
+
+TrueDists = {RNGammaMS(400,40), RNGammaMS(450,45)};
+Contam = RNGammaMS(1000,100);
+ConType = clOutlierModel.Replace;
+
+ModelWithContam = clOutlierModel(TrueDists,prOutlier,Contam,ConType);
+X = ModelWithContam.Random(2000,1);
+
+% Fix the means of the true dist, just for an illustration of fixing parameters.
+parmCodes = ModelWithContam.DefaultParmCodes;
+parmCodes([1 3]) = 'f';
+
+% This is very fast, both because some parameters are fixed and because
+% replacement is a simple mixture distribution without the Convolution
+% or Product components used with Shift & Stretch.
+[EndingVals,fval,exitflag,output] = EstML(ModelWithContam,X,parmCodes)
